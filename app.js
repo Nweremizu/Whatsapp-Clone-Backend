9e170bf997766761b220e4146ba9631442ccf920
Bruno <nweremizu.bruno@stu.cu.edu.ng>
2024-06-11 23:15:40 +0100

    chore: Update CORS origin to allow requests from multiple domains

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const limiter = rateLimit({
 // -------------- Middlewares --------------
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:3000", "https://whatsapp-clone-frontend-pied.vercel.app"]
     credentials: true,
   }),
 ); // Enable All CORS Requests
