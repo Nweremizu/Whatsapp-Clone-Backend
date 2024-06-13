41861ab1dfe8f9000b23023771dbc27afda068fb
Bruno <nweremizu.bruno@stu.cu.edu.ng>
2024-06-13 10:01:10 +0100

    chore: Update CORS origin to include "https://chat-piedd.vercel.app"

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "https://whatsapp-clone-frontend-pied.vercel.app",
-      "chat-piedd.vercel.app",
+      "https://chat-piedd.vercel.app",
     ],
     credentials: true,
   }),
